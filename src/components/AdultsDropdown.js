8b71788eea8b434e40edb6af10162a7bc308a5dd
matheusnoal <matheusnoal2@gmail.com>
2023-06-05 22:17:38 -0300

    setup

---
+++ src/components/AdultsDropdown.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function AdultsDropdown() {
+  return (
+    <div>AdultsDropdown</div>
+  );
+}
---
+++ src/components/KidsDropdown.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function KidsDropdown() {
+  return (
+    <div>KidsDropdown</div>
+  );
+}
